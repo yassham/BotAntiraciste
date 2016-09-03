0c6e77c2a2977a84c859964b0289d9fd485fb06d
YassHam <contact@yassinehamouten.ovh>
2016-09-03 19:16:34 +0200

    Amélioration de la détéction de tweets

--- modules/stream.js
+++ modules/stream.js
@@ -1,19 +1,26 @@
-console.log("[Monitor] "+ monitor);
-console.log("En écoute de racistes ...");
+console.log("[Monitor] "+ monitor+'\n');
+console.log("En écoute de racistes ... \n");
 client.stream('statuses/filter', {track: monitor}, function(stream) {
   stream.on('data', function(event) {
+
     var tweetId = event.id_str;
     var user = event.user.screen_name;
+    var tweet = event.text;
+    console.log("Tweet potentiellement raciste detecte : "+ tweet);
 
-    console.log(event.user.screen_name + '( id : '+tweetId+' ) : ' + event.text);
+    if(tweet.toLowerCase().indexOf(monitor) != -1 && user!="BotAntiraciste"){
+      console.log("Tweet raciste detecte!");
+      console.log(event.user.screen_name + '( id : '+tweetId+' ) : ' + event.text);
 
-    client.post('statuses/update', {status: '@'+user+' Si tu l\'es ! Raciste va !!! #BotAntiraciste',in_reply_to_status_id: tweetId}, function(error, tweet, response) {
-      if (!error) {
-        console.log('Réponse envoyee! Paf dans ta gueule le raciste');
-      }
-    });
+      client.post('statuses/update', {status: '@'+user+' Si tu l\'es, raciste va !!! #BotAntiraciste « '+event.text+' »', in_reply_to_status_id: tweetId}, function(error, tweet, response) {
+        if (!error) {
+          console.log('Réponse envoyee! Paf dans ta gueule le raciste \n');
+        }
+      });
+    }else{
+      console.log("fausse alerte ... \n");
+    }
 
-    console.log(event.user.screen_name);
   });
 
   stream.on('error', function(error) {
